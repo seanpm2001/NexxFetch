2dfae5e5b6c4dec73ad36701982c019738e76af6
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-06-16 20:38:38 -0700

    FETCHFOX_V1.rs
    An archive of the first version of the fetchfox script for this directory.

---
+++ Fetch/Firefox/FETCHFOX/OldVersions/1/1-100/FETCHFOX_V1.rs
@@ -0,0 +1,31 @@
+// Start of script
+// cargo new fetchfox
+// Note: I am not very good at Rust programming, this is just an example for now. Rust programmers who feel like helping are encouraged to improve this project.
+
+fn main() {
+  return fetchFox();
+}
+fn fetch() {
+  // Method coming soon
+}
+fn methods() {
+  str browserLogoASCII.fetch = "(*)";
+  let tabCount.fetch = 0;
+  let ramUsage.fetch = 0;
+  let cpuUsage.fetch = 0.0;
+  let addonCount.fetch = 0;
+  let themeCount.fetch = 0;
+  let extensionCount.fetch = 0;
+  let bookmarkCount.fetch = 0;
+  let cookieCount.fetch = 0;
+  let neofetch.fetch = "sudo apt-install neofetch", return prog:neofetch(); // Not valid
+}
+fn fetchFox() {
+  return methods.fetch();
+}
+/* File info
+* File type: Rust source file (*.rs)
+* File version: 1 (Wednesday, June 16th 2021 at 8:38 pm)
+* Line count (including blank lines and compiler line): 32
+*/
+// End of script
